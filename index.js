5a74dcfe9963af81bf96eb3655146bbebb9a112a
arafay493 <arafay493@gmail.com>
2024-11-26 22:53:20 +0500

    Created error handlers functions.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./config/dbConnect");
 const authRouter = require("./routes/authRoutes");
+const { errorHandler, notFound } = require("./middlewares/errorHandler");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8000;
@@ -8,6 +9,9 @@ const port = process.env.PORT || 8000;
 db();
 app.use(express.json());
 app.use(authRouter);
+
+app.use(errorHandler)
+app.use(notFound)
 app.listen(port, () =>
   console.log("> Server is up and running on port : " + port)
 );
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,29 @@
+//not found
+
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found: ${req.originalUrl()}`);
+  res.status(404);
+  next(error);
+};
+
+//error handling
+
+// const errorHandler = (err, req, res, next) => {
+//   res.status(err.status || 500);
+//   res.json({
+//     error: {
+//       message: err.message,
+//       stack: err.stack,
+//     },
+//   });
+// };
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { errorHandler, notFound };
