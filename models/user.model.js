d6559356d3c39c7bff06906db23bd0b11e586fc3
arafay493 <arafay493@gmail.com>
2024-11-28 11:31:35 +0500

    Hash password and Compare Password done in the user model.

--- models/user.model.js
+++ models/user.model.js
@@ -1,31 +1,46 @@
-const mongoose = require('mongoose'); // Erase if already required
-
+const mongoose = require("mongoose"); // Erase if already required
+const bcrypt = require("bcrypt");
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true,
-        index:true,
-    },
-    lastname:{
-        type:String,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
+  firstname: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  lastname: {
+    type: String,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+// userSchema.methods.comparePassword = async function (candidatePassword) {
+//     return await bcrypt.compare(candidatePassword, this.password);
+// };
+
+userSchema.methods.isPasswordMatched = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
