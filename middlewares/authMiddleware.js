2e1aae8141751d9fdebe9dd1a85d4238f8392dfd
arafay493 <arafay493@gmail.com>
2024-12-02 13:42:58 +0500

    Decoded a user in authMiddleware done.

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,25 @@
+const jwt = require('jsonwebtoken')
+
 const authMiddleware = async (req, res, next) => {
   // Add your authentication logic here
+  // Example:
+//   const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//   try {
+//     req.user = decoded;
+//     next();
+//   } catch (error) {
+//     res.status(401).message({ message: 'Invalid token' });
+//   }
+  // req.user = decoded;
   // For example, validate JWT token
+  // If token is expired, send a 403 Forbidden response
+  // If token is not provided, send a 401 Unauthorized response
   // If token is valid, continue to the next middleware
   // If token is invalid, send a 401 Unauthorized response
-  console.log("Authentication Middleware Running...............");
-  next();
+  console.log("Authentication Middleware Running..............." , decoded);
+//   next();
 };
 
 module.exports = { authMiddleware };
--- models/user.model.js
+++ models/user.model.js
@@ -2,75 +2,78 @@ const mongoose = require("mongoose"); // Erase if already required
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    index: true,
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    lastname: {
+      type: String,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    cart: {
+      type: Array,
+      default: [],
+      // items: {
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   ref: "Product",
+      // },
+    },
+    addresses: {
+      // type: Array,
+      // default: [],
+      // items: {
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   ref: "Address",
+      // },
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+    },
+    wishList: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      // items: {
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   ref: "Product",
+      // },
+    },
   },
-  lastname: {
-    type: String,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  cart: {
-    type: Array,
-    default: [],
-    // items: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "Product",
-    // },
-  },
-  addresses: {
-    // type: Array,
-    // default: [],
-    // items: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "Address",
-    // },
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Address",
-  },
-  wishList: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    // items: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "Product",
-    // },
+  {
+    timestamps: true,
+    // strict: true,
   }
-}, {
-  timestamps: true,
-  // strict: true,
-});
+);
 
 userSchema.pre("save", async function (next) {
-    if (this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 // userSchema.methods.comparePassword = async function (candidatePassword) {
@@ -78,13 +81,17 @@ userSchema.pre("save", async function (next) {
 // };
 
 userSchema.methods.isPasswordMatched = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
-userSchema.methods.generateAuthToken = async function() {
-  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+userSchema.methods.generateAuthToken = async function () {
+  const user = this.toObject();
+  delete user.password;
+  const token = jwt.sign({ ...user }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
   return token;
-}
+};
 
 //Export the model
 module.exports = mongoose.model("User", userSchema);
