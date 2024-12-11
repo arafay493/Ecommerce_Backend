f46dc782afc8241f593d528fd4cd64149b70dac5
arafay493 <arafay493@gmail.com>
2024-12-11 22:28:10 +0500

    Create Product API endpoint Created Successfully.

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,27 +2,49 @@ const Product = require("../models/product.model");
 
 // Create and Save a new Product
 const createProductController = async (req, res, next) => {
-  // Validate request
-  // ...
-  res.send({message: "Product created successfully!"})
-
-  // Create a Product object
-//   const newProduct = new Product({
-//     name: req.body.name,
-//     price: req.body.price,
-//     description: req.body.description,
-//     category: req.body.category,
-//     quantity: req.body.quantity,
-//     image: req.body.image,
-//   });
-
-//   // Save the Product in the database
-//   try {
-//     const data = await newProduct.save();
-//     res.status(201).send({ message: "Product created successfully!", data });
-//   } catch (error) {
-//     res.status(400).send({ message: error.message });
-//   }
+  try {
+    // Validate request
+    if (!req.body.title) {
+      res.status(400);
+      throw new Error("Name must be provided.");
+    }
+    if (!req.body.slug) {
+      res.status(400);
+      throw new Error("Slug must be provided.");
+    }
+    if (!req.body.price) {
+      res.status(400);
+      throw new Error("Price must be provided.");
+    }
+    if (!req.body.quantity) {
+      res.status(400);
+      throw new Error("Quantity must be provided.");
+    }
+    if (!req.body.category) {
+      res.status(400);
+      throw new Error("Category must be provided.");
+    }
+    if (!req.body.description) {
+      res.status(400);
+      throw new Error("Description must be provided.");
+    }
+    // Check if product already exists
+    const productExists = await Product.findOne({ slug: req.body.slug });
+    if (productExists) {
+      res.status(400);
+      throw new Error("Product with the same name already exists.");
+    }
+    // Create a Product object
+    const newProduct = new Product(req.body);
+    const data = await newProduct.save();
+    res.status(201);
+    res.locals.message = "Product created successfully";
+    res.locals.data = data;
+    res.locals.headersSend = true;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports = {createProductController};
+module.exports = { createProductController };
